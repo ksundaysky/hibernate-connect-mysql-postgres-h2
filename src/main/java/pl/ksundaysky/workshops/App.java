25cb529ce536151a962b24a5439c60a5423a70f6
Ola <aleksandra.podorska@gmail.com>
2019-06-04 10:48:43 +0200

    clear log branch fix

--- src/main/java/pl/ksundaysky/workshops/App.java
+++ src/main/java/pl/ksundaysky/workshops/App.java
@@ -4,14 +4,10 @@ import org.hibernate.Session;
 import org.hibernate.Transaction;
 import pl.ksundaysky.workshops.connectors.Connector;
 import pl.ksundaysky.workshops.connectors.H2Connector;
-import pl.ksundaysky.workshops.connectors.MysqlConnector;
-import pl.ksundaysky.workshops.connectors.PostgresConnector;
 import pl.ksundaysky.workshops.model.*;
-import pl.ksundaysky.workshops.query.CriteriaApi;
 
 import java.util.Collections;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 /**
@@ -21,28 +17,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         connect(new H2Connector());
     }
-
-
-//    static void connect(Connector connector){
-//        try(Session session = connector.getSession()){
-//            Transaction transaction = session.beginTransaction();
-//
-//            Person person = new Person("krzysiu","sundaysky",12);
-//            session.save(new Person("krzysiusssss","sundayskyss",13));
-//            session.save(new Person("krzysiusss","sundayskysssss",14));
-//
-//            Umowa umowa1 = new Umowa("umowa podstawowa",person);
-//            session.save(person);
-//            session.save(umowa1);
-//
-//
-//            transaction.commit();
-//
-//            new CriteriaApi().listAll(session,Person.class);
-//
-//
-//        }
-//    }
 
     static void connect(Connector connector) throws Exception {
         try (Session session = connector.getSession()) {
@@ -67,9 +41,6 @@ public class App {
             session.save(wrupek);
 
             transaction.commit();
-
-            List<Book> books = new CriteriaApi().listAll(session, Book.class);
-            books.forEach(System.out::println);
 
         }
     }
