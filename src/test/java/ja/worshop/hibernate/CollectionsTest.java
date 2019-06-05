0556cdb1b689c93f908fadc23a878470f4f4a95b
Agnieszka <atrzewik9412@gmail.com>
2019-06-05 11:56:23 +0200

    Solution of hibernate collections task

---
+++ src/test/java/ja/worshop/hibernate/CollectionsTest.java
@@ -0,0 +1,59 @@
+package ja.worshop.hibernate;
+
+import ja.workshop.hibernate.connectors.H2Connector;
+import ja.workshop.hibernate.connectors.SessionConnector;
+import ja.workshop.hibernate.model.Author;
+import ja.workshop.hibernate.model.Book;
+import ja.workshop.hibernate.model.Genre;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.collection.internal.PersistentSet;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import static org.testng.Assert.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author Agnieszka Trzewik
+ */
+@Test
+public class CollectionsTest {
+
+    private Book book;
+
+    @BeforeMethod
+    public void initialize(){
+        Author author1 = new Author("Anna", "Kowalska");
+        Author author2 = new Author("Maria", "Nowak");
+        Set<Author> authors = new HashSet<>();
+        authors.add(author1);
+        authors.add(author2);
+        book = new Book("Fikcyjna podróż", authors, Genre.FANTASY);
+    }
+
+    @Test
+    public void givenCollection_ThenReturnJavaHashSet(){
+
+        //Assert
+        assertEquals(book.getAuthors().getClass(), HashSet.class);
+    }
+
+    @Test
+    public void givenCollection_ThenReturnHibernatePersistentSet(){
+
+        //Arrange
+        SessionConnector sessionConnector = new H2Connector();
+        Session session = sessionConnector.getSession();
+        Transaction transaction = session.beginTransaction();
+
+        //Act
+        session.persist(book);
+
+        session.close();
+
+        //Assert
+        assertEquals(book.getAuthors().getClass(), PersistentSet.class);
+    }
+}
