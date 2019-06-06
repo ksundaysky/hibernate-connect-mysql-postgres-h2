a528bee8717886515937c8fa5f269cefb45b633a
Kapaika <bartosz.kupajski@gmail.com>
2019-06-06 12:23:44 +0200

    Code fix
    -create inner class
    -divide main method into small parts
    -name threads
    -change sleep method

--- src/main/java/ja/workshop/hibernate/App.java
+++ src/main/java/ja/workshop/hibernate/App.java
@@ -14,69 +14,90 @@ import java.util.concurrent.TimeUnit;
 public class App {
     public static void main(String[] args) throws Exception {
 
-        Author authorJuliusz = new Author("Juliusz", "Słowacki");
-        Book kordian = new Book("Kordian", Set.of(authorJuliusz), Genre.CLASSIC);
+        dodajKordiana();
+
+        tworzenieWalczącychWątków(1L);
+
+    }
+
+
+
+    private static void tworzenieWalczącychWątków(long id) {
+        Thread wątekPrzegrywający = new Thread(new WątekWalczącyPrzegrywający(id),"WątekPrzegrywający");
+        Thread wątekWygrywający = new Thread(new WątekWalczącyWygrywający(id),"WątekWygrywający");
+
+        wątekPrzegrywający.start();
+        wątekWygrywający.start();
+    }
+
+    private static void dodajKordiana() {
+        Author pisarzJuliusz = new Author("Juliusz", "Słowacki");
+        Book ksiażkaKordian = new Book("Kordian", Set.of(pisarzJuliusz), Genre.CLASSIC);
+        Bookstore ksiegarniaPodGlobusem = new Bookstore("Ksiegarnia Pod Globusem");
         Transaction transaction = null;
 
         try (Session session = new H2Connector().getSession()) {
             transaction = session.beginTransaction();
 
-            session.save(kordian);
-            //TODO: new Bookstore jako zmienna
-            BookstoreBook summary = new BookstoreBook(new Bookstore("Ksiegarnia"), kordian, 12);
+            session.save(ksiażkaKordian);
+            BookstoreBook summary = new BookstoreBook(ksiegarniaPodGlobusem, ksiażkaKordian, 12);
             session.save(summary);
 
             transaction.commit();
-            //TODO: co to za Exception? nie ma nic konkretniejszego?
         } catch (Exception e) {
             if (transaction != null) transaction.rollback();
-            //TODO: łapiesz i rzucasz?!
             throw e;
         }
+    }
 
-        Long personId = 1L;
+    private static class WątekWalczącyPrzegrywający implements Runnable{
 
-        //TODO: można zaimplementować klaskę od Runnable, która przyjmuje czas spania, bo reszta jest taka sama w run()
-        //TODO: nazwy dla wąteczków
-        Thread t1 = new Thread(new Runnable() {
-            Session session1 = new H2Connector().getSession();
-            Transaction tx = null;
+        Session session1 = new H2Connector().getSession();
+        Transaction tx = null;
+        long personId;
 
-            @Override
-            public void run() {
-                BookstoreBook summary = session1.get(BookstoreBook.class, personId);
-                if (summary != null) {
-                    tx = session1.beginTransaction();
+        public WątekWalczącyPrzegrywający(long personId) {
+            this.personId = personId;
+        }
 
-                    try {
-                        //TODO: nowsza wersja sleepa - TimeUnit.SECONDS.sleep(5000);
-                        Thread.sleep(5000);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                    summary.setPrice(18);
-                    session1.update(summary);
-                    tx.commit();
+        @Override
+        public void run() {
+            BookstoreBook summary = session1.get(BookstoreBook.class, personId);
+            if (summary != null) {
+                tx = session1.beginTransaction();
+
+                try {
+                    TimeUnit.SECONDS.sleep(5);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
                 }
+                summary.setPrice(18);
+                session1.update(summary);
+                tx.commit();
             }
-        });
-        Thread t2 = new Thread(new Runnable() {
-            Session session2 = new H2Connector().getSession();
-            Transaction tx = null;
+        }
+    }
 
-            @Override
-            public void run() {
-                BookstoreBook summary = session2.get(BookstoreBook.class, personId);
-                if (summary != null) {
-                    tx = session2.beginTransaction();
-                    summary.setPrice(25);
-                    session2.update(summary);
-                    tx.commit();
-                }
+    private  static class WątekWalczącyWygrywający implements Runnable {
+
+        Session session2 = new H2Connector().getSession();
+        Transaction tx = null;
+        long personId;
+
+        public WątekWalczącyWygrywający(long personId) {
+            this.personId = personId;
+        }
+
+        @Override
+        public void run() {
+            BookstoreBook summary = session2.get(BookstoreBook.class, personId);
+            if (summary != null) {
+                tx = session2.beginTransaction();
+                summary.setPrice(25);
+                session2.update(summary);
+                tx.commit();
             }
-        });
-//        t1.start();
-//        t2.start();
-
+        }
     }
 }
+
--- src/main/java/ja/workshop/hibernate/connectors/H2Connector.java
+++ src/main/java/ja/workshop/hibernate/connectors/H2Connector.java
@@ -16,7 +16,7 @@ public class H2Connector extends Connector {
         settings.put(Environment.USER, "sa");
         settings.put(Environment.PASS, "");
         settings.put(Environment.DIALECT, "org.hibernate.dialect.H2Dialect");
-        settings.put(Environment.HBM2DDL_AUTO, "update");
+        settings.put(Environment.HBM2DDL_AUTO, "create-drop");
         settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
         return settings;
     }
