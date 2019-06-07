d13aeadb02290beaa3773ba7c0e84bf5528e7be6
Krzysztof Kramarz <krzysztof.kramarz@silvermedia.pl>
2019-06-07 18:42:26 +0200

    zadanie konfiguracja entityManagera

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # hibernate-connect-mysql-postgres-h2
 
-* na masterku jest szysko a na innych galeziach nie 
-* u siebie rob jak u siebie
+Zadanie polega na stworzeniu odpowiedniego pliku konfiguracyjnego xml. Plik ma umożliwić konfigurację Entity Managera
+dla połączenia się z jedną z baz danych (H2 lub PostgreSQL lub MySQL).
+
--- src/main/java/pl/ksundaysky/workshops/App.java
+++ src/main/java/pl/ksundaysky/workshops/App.java
@@ -1,10 +1,5 @@
 package pl.ksundaysky.workshops;
 
-import pl.ksundaysky.workshops.connectors.ConnectorManager;
-import pl.ksundaysky.workshops.connectors.EntityManager.EntityManagerConnectorManager;
-import pl.ksundaysky.workshops.connectors.EntityManager.H2Connector;
-import pl.ksundaysky.workshops.connectors.EntityManager.MysqlConnector;
-import pl.ksundaysky.workshops.connectors.EntityManager.PostgreSQLConnector;
 import pl.ksundaysky.workshops.connectors.SessionCloseException;
 import pl.ksundaysky.workshops.connectors.SessionInitializationException;
 import pl.ksundaysky.workshops.model.Author;
@@ -14,36 +9,39 @@ import pl.ksundaysky.workshops.model.Genre;
 import java.util.Set;
 
 /**
+ * Odkomentuj te linijki,
+ * dla których skonfigurowłeś Entity Managera
+ *
  * @author Kamil Rojek
  */
 public class App {
     public static void main(String[] args) {
 
-        ConnectorManager<MysqlConnector> connectorManagerMySQL = EntityManagerConnectorManager.of(new MysqlConnector());
-        ConnectorManager<PostgreSQLConnector> connectorManagerPostgreSQL = EntityManagerConnectorManager.of(new PostgreSQLConnector());
-        ConnectorManager<H2Connector> connectorManagerH2 = EntityManagerConnectorManager.of(new H2Connector());
+//        ConnectorManager<MysqlConnector> connectorManagerMySQL = EntityManagerConnectorManager.of(new MysqlConnector());
+//        ConnectorManager<PostgreSQLConnector> connectorManagerPostgreSQL = EntityManagerConnectorManager.of(new PostgreSQLConnector());
+//        ConnectorManager<H2Connector> connectorManagerH2 = EntityManagerConnectorManager.of(new H2Connector());
 
-        Author author = new Author("Proba", "Finalna3");
-        Set<Author> authors = Set.of(new Author("Jan", "Samoradzik"));
-        Book book = new Book("asd", authors, Genre.CLASSIC);
+        Author author = new Author("Julian", "Tuwim");
+        Set<Author> authors = Set.of(new Author("Stanisław", "Lem"), new Author("Edmund", "Niziurski"));
+        Book book = new Book("Siódme wtajemniczenie", authors, Genre.CLASSIC);
 
-        connectorManagerMySQL.addRecords(author);
-        connectorManagerMySQL.addRecords(authors);
-        connectorManagerMySQL.addRecords(book);
-
-        connectorManagerPostgreSQL.addRecords(author);
-        connectorManagerPostgreSQL.addRecords(authors);
-        connectorManagerPostgreSQL.addRecords(book);
-
-        connectorManagerH2.addRecords(author);
-        connectorManagerH2.addRecords(authors);
-        connectorManagerH2.addRecords(book);
+//        connectorManagerMySQL.addRecords(author);
+//        connectorManagerMySQL.addRecords(authors);
+//        connectorManagerMySQL.addRecords(book);
+//
+//        connectorManagerPostgreSQL.addRecords(author);
+//        connectorManagerPostgreSQL.addRecords(authors);
+//        connectorManagerPostgreSQL.addRecords(book);
+//
+//        connectorManagerH2.addRecords(author);
+//        connectorManagerH2.addRecords(authors);
+//        connectorManagerH2.addRecords(book);
 
 
         try {
-            connectorManagerMySQL.commitAndClose();
-            connectorManagerPostgreSQL.commitAndClose();
-            connectorManagerH2.commitAndClose();
+//            connectorManagerMySQL.commitAndClose();
+//            connectorManagerPostgreSQL.commitAndClose();
+//            connectorManagerH2.commitAndClose();
         } catch (SessionInitializationException e) {
             e.getMessage();
         } catch (SessionCloseException e) {
