550652c906fa10e16229ad0be4d6932ac5f8bc38
Ola <aleksandra.podorska@gmail.com>
2019-06-10 09:53:58 +0200

    add two main classes with example of hibernate loading

---
+++ src/main/java/ja/workshop/hibernate/AppEager.java
@@ -0,0 +1,55 @@
+package ja.workshop.hibernate;
+
+import ja.workshop.hibernate.connectors.H2Connector;
+import ja.workshop.hibernate.model.Author;
+import ja.workshop.hibernate.model.Book;
+import ja.workshop.hibernate.model.Genre;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+/**
+ * Here is an example of eager loading.
+ * TODO: start program and see what will happen
+ * TODO: than comment 49 line, uncomment 46 and start again
+ * What did you see? What queries did Hibernate?
+ * --> When you finish, go to AppLazy class
+ *
+ * @author Ola Podorska
+ */
+public class AppEager {
+    public static void main(String[] args) {
+        Session session = new H2Connector().getSession();
+        Transaction transaction = session.beginTransaction();
+        Author author1 = new Author("imie1", "nazwisko1");
+        Author author2 = new Author("imie2", "nazwisko2");
+        Author author3 = new Author("imie3", "nazwisko3");
+        Book book1 = new Book("tytul1", new HashSet<>(Arrays.asList(author1)), Genre.CLASSIC);
+        Book book2 = new Book("tytul2", new HashSet<>(Arrays.asList(author1)), Genre.CLASSIC);
+        Book book3 = new Book("tytul3", new HashSet<>(Arrays.asList(author2)), Genre.CLASSIC);
+
+        session.save(author1);
+        session.save(author2);
+        session.save(author3);
+        session.save(book1);
+        session.save(book2);
+        session.save(book3);
+
+        transaction.commit();
+
+        session.close();
+
+        session = new H2Connector().getSession();
+        transaction = session.beginTransaction();
+        Book book = session.get(Book.class, 1L);
+        transaction.commit();
+//        session.close();
+        System.out.println("--> my book: " + book);
+        System.out.println("--> authors: " + book.getAuthors());
+        session.close();
+
+    }
+
+}
