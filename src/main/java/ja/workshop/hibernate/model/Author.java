3dd545f79a3316dd244c7c0093e2e8ba5e7c4089
Ola <aleksandra.podorska@gmail.com>
2019-06-11 10:57:15 +0200

    Update classes: - change Long id to long

--- src/main/java/ja/workshop/hibernate/model/Author.java
+++ src/main/java/ja/workshop/hibernate/model/Author.java
@@ -12,7 +12,7 @@ public class Author {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false)
-    private Long id;
+    private long id;
 
     @Column(name = "name", nullable = false)
     private String name;
