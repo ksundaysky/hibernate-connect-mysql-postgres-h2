c10afbb31c6f5ad621849787832db19812275504
Krzysztof Kramarz <krzysztof.kramarz@silvermedia.pl>
2019-06-11 12:03:41 +0200

    Update classes: - refactor App.java - delete package

--- src/main/java/ja/workshop/hibernate/App.java
+++ src/main/java/ja/workshop/hibernate/App.java
@@ -1,5 +1,12 @@
 package ja.workshop.hibernate;
 
+import ja.workshop.hibernate.connectors.ConnectorManager;
+import ja.workshop.hibernate.connectors.EntityManager.EntityManagerConnectorManager;
+import ja.workshop.hibernate.connectors.EntityManager.H2Connector;
+import ja.workshop.hibernate.connectors.EntityManager.MysqlConnector;
+import ja.workshop.hibernate.connectors.EntityManager.PostgreSQLConnector;
+import ja.workshop.hibernate.connectors.SessionCloseException;
+import ja.workshop.hibernate.connectors.SessionInitializationException;
 import ja.workshop.hibernate.model.Author;
 import ja.workshop.hibernate.model.Book;
 import ja.workshop.hibernate.model.Genre;
@@ -15,33 +22,33 @@ import java.util.Set;
 public class App {
     public static void main(String[] args) {
 
-//        ConnectorManager<MysqlConnector>   connectorManagerMySQL = EntityManagerConnectorManager.of(new MysqlConnector());
-//        ConnectorManager<PostgreSQLConnector> connectorManagerPostgreSQL = EntityManagerConnectorManager.of(new PostgreSQLConnector());
-//        ConnectorManager<H2Connector> connectorManagerH2 = EntityManagerConnectorManager.of(new H2Connector());
+        ConnectorManager<MysqlConnector> connectorManagerMySQL = EntityManagerConnectorManager.of(new MysqlConnector());
+        ConnectorManager<PostgreSQLConnector> connectorManagerPostgreSQL = EntityManagerConnectorManager.of(new PostgreSQLConnector());
+        ConnectorManager<H2Connector> connectorManagerH2 = EntityManagerConnectorManager.of(new H2Connector());
 
         Author author = new Author("Julian", "Tuwim");
         Set<Author> authors = Set.of(new Author("Stanisław", "Lem"), new Author("Edmund", "Niziurski"));
         Book book = new Book("Siódme wtajemniczenie", authors, Genre.CLASSIC);
 
-//        connectorManagerMySQL.addRecords(author);
-//        connectorManagerMySQL.addRecords(authors);
-//        connectorManagerMySQL.addRecords(book);
-//
-//        connectorManagerPostgreSQL.addRecords(author);
-//        connectorManagerPostgreSQL.addRecords(authors);
-//        connectorManagerPostgreSQL.addRecords(book);
-//
-//        connectorManagerH2.addRecords(author);
-//        connectorManagerH2.addRecords(authors);
-//        connectorManagerH2.addRecords(book);
+        connectorManagerMySQL.addRecords(author);
+        connectorManagerMySQL.addRecords(authors);
+        connectorManagerMySQL.addRecords(book);
+
+        connectorManagerPostgreSQL.addRecords(author);
+        connectorManagerPostgreSQL.addRecords(authors);
+        connectorManagerPostgreSQL.addRecords(book);
+
+        connectorManagerH2.addRecords(author);
+        connectorManagerH2.addRecords(authors);
+        connectorManagerH2.addRecords(book);
 
 
-//        try {
-//            connectorManagerMySQL.commitAndClose();
-//            connectorManagerPostgreSQL.commitAndClose();
-//            connectorManagerH2.commitAndClose();
-//        } catch (SessionInitializationException | SessionCloseException e) {
-//            e.getMessage();
-//        }
+        try {
+            connectorManagerMySQL.commitAndClose();
+            connectorManagerPostgreSQL.commitAndClose();
+            connectorManagerH2.commitAndClose();
+        } catch (SessionInitializationException | SessionCloseException e) {
+            e.getMessage();
+        }
     }
 }
