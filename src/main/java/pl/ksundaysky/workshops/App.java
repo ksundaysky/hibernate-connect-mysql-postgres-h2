46004a3e6bca3cbbb79343a3445020af73e82977
Kamil Rojek <kamroj@gmail.com>
2019-06-11 08:28:30 +0200

    Fix deleting problem

--- src/main/java/pl/ksundaysky/workshops/App.java
+++ src/main/java/pl/ksundaysky/workshops/App.java
@@ -17,16 +17,12 @@ public class App {
     public static void main(String[] args) {
         Author author = new Author("Kamil", "R");
         Set<Author> authors = Set.of(new Author("Jan", "Brzechwa"), new Author("OLA", "POD"));
-        Book book = new Book("BOOK", authors, Genre.CLASSIC);
 
-        Author authorModification = new Author("ZMIENIONE", "ZMIENIONE");
-        authorModification.setId(2L);
 
         connect(new H2Connector())
                 .openCrudSession(new CrudMethods())
                 .updateRecord(author)
                 .updateRecord(authors)
-                .updateRecord(book)
                 .commitAndClose();
 
         Author authorKamil = (Author) connect(new H2Connector())
