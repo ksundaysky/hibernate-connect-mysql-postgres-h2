550e29c1bae9bf4dd6e47ddd8fd8fbf2fd86c619
Krzysiek <krzysztof.niedzielski96@gmail.com>
2019-06-12 13:51:31 +0200

    query-task new way

---
+++ src/main/java/ja/workshops/hibernate/parts/queries/query/SQLQuery.java
@@ -0,0 +1,25 @@
+package ja.workshops.hibernate.parts.queries.query;
+
+import ja.workshops.hibernate.parts.model.Author;
+import org.hibernate.Session;
+
+import java.util.List;
+
+/**
+ * Documentation:
+ * Session : https://docs.jboss.org/hibernate/orm/5.4/javadocs/
+ * Tip: createSqlQuery()
+ * createSqlQuery returns List<Object[]> so we have to parse Object[] to Author object.
+ * @author krzysztof.niedzielski
+ */
+public class SQLQuery {
+
+    public List<Author> listAll(Session session) {
+        return null;
+    }
+
+    public List<Author> listAllAuthorsWithSpecifiedName(Session session, String name) {
+        return null;
+    }
+
+}
